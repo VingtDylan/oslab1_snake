fe8bcc381d9e341fd1c0ea479841aa2751add5fb
tracer <tracer@tracer.org>
2018-03-30 20:57:44 +0800

    > build cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -62,7 +62,7 @@ int main() {
   return 0;
 }
 
-
+/*
 static void input_test(_Device *dev) {
   printf("Input device test skipped.\n");
 }
@@ -162,3 +162,4 @@ static void ata_test(_Device *dev) {
     printf("\n");
   }
 }
+*/
