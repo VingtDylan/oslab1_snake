eed0ffdb8e0a9b0cca73f244f4b7c72e0049a069
tracer <tracer@tracer.org>
2018-03-31 14:44:35 +0800

    > build cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -197,6 +197,114 @@ size_t strnlen(const char *s,size_t count){
     return c-s;
 }
 
+static noinline_for_stack
+char *number(char *buf, char *end, unsigned long long num,struct printf_spec spec){
+     static const char digits[16] = "0123456789ABCDEF";         
+     char tmp[66];        
+     char sign;
+     char locase;
+     int need_pfx = ((spec.flags & SPECIAL) && spec.base != 10);
+     int i;
+
+     locase = (spec.flags & SMALL)
+     if (spec.flags & LEFT)
+         spec.flags &= ~ZEROPAD;
+     sign = 0;
+     if (spec.flags & SIGN) {/
+         if ((signed long long)num < 0) {
+                sign = '-';
+                num = -(signed long long)num;
+                spec.field_width--;
+         } else if (spec.flags & PLUS) {
+                sign = '+';
+                spec.field_width--;
+         } else if (spec.flags & SPACE) {
+                spec.field_width--;
+         }
+     }
+     if (need_pfx) {                
+         spec.field_width--;                 
+         if (spec.base == 16)
+               spec.field_width--;
+     }
+
+     i = 0;
+     if (num == 0)
+         tmp[i++] = '0';
+      
+     else if (spec.base != 10) {
+         int mask = spec.base - 1; 
+         int shift = 3;
+
+         if (spec.base == 16)
+               shift = 4;
+               do {
+                     tmp[i++] = (digits[((unsigned char)num) & mask] | locase);
+                     num >>= shift;
+               } while (num);
+     }else{
+          i = put_dec(tmp, num) - tmp;
+     }
+
+     if (i > spec.precision)
+          spec.precision = i;
+       
+     spec.field_width -= spec.precision;
+     if (!(spec.flags & (ZEROPAD+LEFT))) {
+         while (--spec.field_width >= 0) {
+               if (buf < end)
+                     *buf = ' ';
+               ++buf;
+               }
+     }       
+        
+     if (sign) {
+         if (buf < end)
+                *buf = sign;
+         ++buf;
+     }        
+        
+     if (need_pfx) {
+         if (buf < end)
+                *buf = '0';
+         ++buf;
+         if (spec.base == 16) {
+                if (buf < end)
+                       *buf = ('X' | locase);
+                ++buf;
+         }
+      }
+       
+      if (!(spec.flags & LEFT)) {
+         char c = (spec.flags & ZEROPAD) ? '0' : ' ';
+                while (--spec.field_width >= 0) {
+                       if (buf < end)
+                           *buf = c;
+                       ++buf;
+                }
+        }
+       
+        while (i <= --spec.precision) {
+                if (buf < end)
+                       *buf = '0';
+                ++buf;
+        }  
+         
+        while (--i >= 0) {
+                if (buf < end)
+                        *buf = tmp;
+                ++buf;
+        }
+       
+        while (--spec.field_width >= 0) {
+                if (buf < end)
+                        *buf = ' ';
+                ++buf;
+        }
+
+        return buf;
+}
+
 int vsprintf(char *buf, const char *fmt, va_list args)
 {
     int len;
@@ -385,7 +493,7 @@ int vsprintf(char *buf, const char *fmt, va_list args)
         }
         str = number(str, num, base, field_width, precision, flags);
     }
-    *str = '/0';
+    *str = '\0';
     return str-buf;
 }
 
