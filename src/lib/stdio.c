87ad214a3659c2315690ed46d09b4c735c65833a
tracer <tracer@tracer.org>
2018-03-31 14:58:38 +0800

    > build cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -199,6 +199,89 @@ size_t strnlen(const char *s,size_t count){
 
 
 
+static char * number(char * str, unsigned long long num, int base, int size, int precision, int type){
+
+    char c,sign,tmp[66];
+    const char *digits="0123456789abcdefghijklmnopqrstuvwxyz";
+
+    int i;
+
+    if (type & LARGE)
+        digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (type & LEFT)
+        type &= ~ZEROPAD;
+
+    if (base < 2 || base > 36)
+        return 0;
+
+    c = (type & ZEROPAD) ? '0' : ' ';
+    sign = 0;
+    if (type & SIGN) {
+        if ((signed long long)num < 0) {
+            sign = '-';
+            num = - (signed long long)num;
+            size--;
+        } else if (type & PLUS) {
+            sign = '+';
+            size--;
+        } else if (type & SPACE) {
+            sign = ' ';
+            size--;
+        }
+     }
+
+     if (type & SPECIAL) {
+         if (base == 16)
+             size -= 2;
+         else if (base == 8)
+             size--;
+     }
+
+     i = 0;
+
+     if (num == 0)
+         tmp[i++]='0';
+
+     else while (num != 0) {
+         tmp[i++] = digits[do_div(num, base)];
+     }
+
+     if (i > precision)
+         precision = i;
+
+     size -= precision;
+
+     if (!(type&(ZEROPAD+LEFT)))
+          while(size-->0)
+               *str++ = ' ';
+     if (sign)
+           *str++ = sign;
+
+     if (type & SPECIAL) {
+           if (base==8)
+               *str++ = '0';
+           else if (base==16) {
+               *str++ = '0';
+               *str++ = digits[33];
+           }
+     }
+
+     if (!(type & LEFT))
+           while (size-- > 0)
+                *str++ = c;
+           while (i < precision--)
+                *str++ = '0';
+           while (i-- > 0)
+                *str++ = tmp[i];
+           while (size-- > 0)
+                *str++ = ' ';
+             return str;
+
+     }
+
+
+
 int vsprintf(char *buf, const char *fmt, va_list args)
 {
     int len;
@@ -391,6 +474,7 @@ int vsprintf(char *buf, const char *fmt, va_list args)
     return str-buf;
 }
 
+static char sprint_buf[104];
 
 int printf(const char *fmt, ...)
 {
