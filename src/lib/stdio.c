c835b10ad37385c33f4b058202621ac342c5ddc0
tracer <tracer@tracer.org>
2018-03-31 14:21:59 +0800

    > build cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -97,6 +97,7 @@ int snprintf(char *buf,size_t size,const char *fmt,...){
     return --index;    
 }
 
+/*
 void printch(const char ch){
     _putc(ch);
 }
@@ -172,4 +173,233 @@ int printf(const char *fmt,... ){
     va_end(ap);
     return 0;
 }
+*/
+
+int vsprintf(char *buf, const char *fmt, va_list args)
+{
+    int len;
+    unsigned long long num;
+    int i, base;
+    char * str;
+    const char *s;
+
+    int flags;        
+
+    int field_width;    
+    int precision;       
+    int qualifier;       
+
+    for (str=buf ; *fmt ; ++fmt)
+    {
+        if (*fmt != '%') 
+        {
+            *str++ = *fmt;
+            continue;
+        }
+        flags = 0;
+        repeat:
+            ++fmt;      
+            switch (*fmt)
+            {
+                case '-': flags |= LEFT; goto repeat;
+                case '+': flags |= PLUS; goto repeat;
+                case ' ': flags |= SPACE; goto repeat;
+                case '#': flags |= SPECIAL; goto repeat;
+                case '0': flags |= ZEROPAD; goto repeat;
+            }
+
+        field_width = -1;
+        if ('0' <= *fmt && *fmt <= '9')
+            field_width = skip_atoi(&fmt);
+        else if (*fmt == '*')
+        {
+            ++fmt;
+            field_width = va_arg(args, int);
+            if (field_width < 0) {
+                field_width = -field_width;
+                flags |= LEFT;
+            }
+        }
+
+ 
+        precision = -1;
+        if (*fmt == '.')
+        {
+            ++fmt;   
+            if ('0' <= *fmt && *fmt <= '9')
+                precision = skip_atoi(&fmt);
+            else if (*fmt == '*')
+            {
+                ++fmt;
+
+                precision = va_arg(args, int);
+            }
+            if (precision < 0)
+                precision = 0;
+        }
+
+        qualifier = -1;
+        if (*fmt == 'l' && *(fmt + 1) == 'l')
+        {
+            qualifier = 'q';
+            fmt += 2;
+        }
+        else if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L'|| *fmt == 'Z')
+        {
+            qualifier = *fmt;
+            ++fmt;
+        }
+
+        base = 10;
+   
+        switch (*fmt)
+        {
+            case 'c':
+                if (!(flags & LEFT))
+                    while (--field_width > 0)
+                    *str++ = ' ';
+                    *str++ = (unsigned char) va_arg(args, int);
+                    while (--field_width > 0)
+                    *str++ = ' ';
+                    continue;
+
+            case 's':
+                s = va_arg(args, char *);
+                if (!s)
+                    s = "";
+
+                    len = strnlen(s, precision);
+
+                    if (!(flags & LEFT))
+                    while (len < field_width--)
+                    *str++ = ' ';
+                    for (i = 0; i < len; ++i)
+                    *str++ = *s++;
+                    while (len < field_width--)
+                    *str++ = ' ';
+                    continue;
+            case 'p':
+                if (field_width == -1)
+                {
+                    field_width = 2*sizeof(void *);
+                    flags |= ZEROPAD;
+                }
+                str = number(str,(unsigned long) va_arg(args, void *), 16,
+                        field_width, precision, flags);
+                continue;
+
+            case 'n':
+            if (qualifier == 'l')
+            {
+                long * ip = va_arg(args, long *);
+                *ip = (str - buf);
+            }
+            else if (qualifier == 'Z')
+            {
+                size_t * ip = va_arg(args, size_t *);
+                *ip = (str - buf);
+            }
+            else
+            {
+                int * ip = va_arg(args, int *);
+                *ip = (str - buf);
+            }
+            continue;
+            case '%':
+            *str++ = '%';
+            continue;
+
+            case 'o':
+            base = 8;
+            break;
+
+            case 'X':
+            flags |= LARGE;
+            case 'x':
+            base = 16;
+            break;
+            case 'd':
+            case 'i':
+            flags |= SIGN;
+            case 'u':
+            break;
+
+            default:
+            *str++ = '%';
+            if (*fmt)
+                *str++ = *fmt;
+            else
+                --fmt;
+            continue;
+        }
+
+        if (qualifier == 'l')
+        {
+            num = va_arg(args, unsigned long);
+            if (flags & SIGN)
+                num = (signed long) num;
+        }
+        else if (qualifier == 'q')
+        {
+            num = va_arg(args, unsigned long long);
+            if (flags & SIGN)
+                num = (signed long long) num;
+        }
+        else if (qualifier == 'Z')
+        {
+            num = va_arg(args, size_t);
+        }
+        else if (qualifier == 'h')
+        {
+            num = (unsigned short) va_arg(args, int);
+            if (flags & SIGN)
+                num = (signed short) num;
+        }
+        else
+        {
+            num = va_arg(args, unsigned int);
+            if (flags & SIGN)
+                num = (signed int) num;
+        }
+        str = number(str, num, base, field_width, precision, flags);
+    }
+    *str = '/0';
+    return str-buf;
+}
+
+
+int printf(const char *fmt, ...)
+{
+    va_list args;
+    int n;
+    va_start(args, fmt);
+    n = vsprintf(sprint_buf, fmt, args);
+    va_end(args);
+    if (console_ops.write)
+        console_ops.write(sprint_buf, n);
+    return n;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
