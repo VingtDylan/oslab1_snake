9c597d5a6261d8d5857234b5e33eee94e7acd425
tracer <tracer@tracer.org>
2018-04-01 15:54:10 +0800

    > run cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -16,19 +16,20 @@
 }
 */
 
-//static void input_test(_Device *dev);
-//static void timer_test(_Device *dev);
-//static void video_test(_Device *dev);
-//static void pciconf_test(_Device *dev);
-//static void ata_test(_Device *dev);
+static void input_test(_Device *dev);
+static void timer_test(_Device *dev);
+static void video_test(_Device *dev);
+static void pciconf_test(_Device *dev);
+static void ata_test(_Device *dev);
 
 int main() {
-  /*
+  
   if (_ioe_init() != 0) _halt(1);
   printf("_heap = [%08x, %08x)\n", _heap.start, _heap.end);
   for (int n = 1; ; n++) {
     _Device *dev = _device(n);
     if (!dev) break;
+    printf("%d\n............",n);
     printf("* Device: %s\n", dev->name);
     switch (dev->id) {
       case _DEV_INPUT: input_test(dev); break;
@@ -38,12 +39,13 @@ int main() {
       case _DEV_ATA0: ata_test(dev); break;
     }
     printf("\n");
-  }*/
+  }
+
   main_loop();
   return 0;
 } 
 
-/*
+
 static void input_test(_Device *dev) {
   printf("Input device test skipped.\n");
 }
@@ -142,4 +144,4 @@ static void ata_test(_Device *dev) {
     printf("\n");
   }
 }
-*/
+
