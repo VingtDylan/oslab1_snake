1de3293548fa14594ff5ab18eb3429cc949d04de
tracer <tracer@tracer.org>
2018-04-01 14:24:38 +0800

    > run cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

---
+++ src/device.c
@@ -0,0 +1,163 @@
+#include <am.h>
+#include <amdev.h>
+#include <tool.h>
+#include <lib.h>
+#include <type.h>
+#include <stdio.h>
+#include <assert.h>
+
+/*parameter
+_DEV_PERFNT  0x0000ac01
+_DEV_INPUT   0x0000ac02
+_DEV_TIMER   0x0000ac03
+_DEV_VIDEO   0x0000ac04
+_DEV_PCICONF 0x00000080
+_DEV_ATA0    0x00000dd0
+_DEV_ATA1    0x00000dd1
+*/
+
+static _Device *perfnt = NULL;
+static _Device *input = NULL;
+static _Device *timer = NULL;
+static _Device *video = NULL;
+static _Device *pciconf = NULL;
+static _Device *ata0=NULL;
+static _Device *ata1=NULL;
+
+_Device *get_device(uint32_t id) {
+  _Device *dev;
+  for (int n = 1; (dev = _device(n)); n++) 
+    if (dev->id == id)
+      return dev;
+  return NULL;
+}
+
+void init_perfnt(){
+  perfnt=get_device(_DEV_PERFNT);
+}
+
+void init_input(){
+  input=get_device(_DEV_INPUT);
+}
+
+void init_timer(){
+  timer=get_device(_DEV_TIMER);
+}
+
+void init_video(){
+  video=get_device(_DEV_VIDEO);
+}
+ 
+void init_pciconf(){
+  pcicon =get_device(_DEV_PCICONF);
+}
+
+void init_ata0(){
+  ata0=get_device(_DEV_ATA0);
+}
+
+void init_ata1(){
+  ata1=get_device(_DEV_ATA1);
+}
+
+void writeb(_Device *dev, uint32_t reg, uint8_t res) {
+  dev->write(reg, &res, 1);
+}
+
+uint32_t uptime() {
+  if (!timer) 
+    init_timer();
+
+  _UptimeReg boottime;
+  timer->read(_DEVREG_TIMER_UPTIME, &boottime, sizeof(boottime));
+  return boottime.lo;
+}
+
+void read_key(int *key, int *down) {
+  if (!input)
+    init_input();
+
+  _KbdReg pressed;
+  input->read(_DEVREG_INPUT_KBD, &pressed, sizeof(pressed));
+  if (key)
+    *key = kbd.keycode;
+  if (down)
+    *down = kbd.keydown;
+}
+
+void draw_rect(uint32_t *pixels, int x, int y, int w, int h) {
+  if (!video) 
+    init_video();
+  
+  _FBCtlReg ctl;
+  ctl.x = x;
+  ctl.y = y;
+  ctl.w = w;
+  ctl.h = h;
+  ctl.sync = 1;
+  ctl.pixels = pixels;
+  video->write(_DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
+}
+
+void draw_sync() {
+  // to be compledted
+}
+
+int screen_width() {
+  if (!video) 
+    init_video();
+
+  _VideoInfoReg video;
+  video->read(_DEVREG_VIDEO_INFO, &video, sizeof(video));
+  return info.width;
+}
+
+int screen_height() {
+  if (!video) 
+    init_video();
+
+  _VideoInfoReg video;
+  video->read(_DEVREG_VIDEO_INFO, &video, sizeof(video));
+  return info.height;
+}
+
+uint32_t read_pciconf(_Device *dev,uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
+  if (!pciconf)
+    init_pciconf();
+  uint32_t res;
+  dev->read(_DEVREG_PCICONF(bus,slot,func,offset),&res,4);
+  return res;
+}
+
+void uint8_t readb(_Device *dev,uint32_t reg){
+  uint8_t res;
+  dev->read(reg,&res,1);
+  return res;
+}
+
+void uint32_t readl(_Device *dev,uint32_t reg){
+  uint32_t res;
+  dev->read(reg,&res,4);
+  return res;
+}
+
+void writeb(_Device *dev,uint32_t reg,uint8_tres){
+  dev->write(reg,%res,1);
+}
+
+void read_disk(void *dst, int offset) {
+  if (!ata0)
+    init_ata0();
+
+  while ((readb(ata0, _DEVREG_ATA_STATUS) & 0xc0) != 0x40);
+  writeb(ata0, _DEVREG_ATA_NSECT,  1);
+  writeb(ata0, _DEVREG_ATA_SECT,   offset);
+  writeb(ata0, _DEVREG_ATA_CYLOW,  offset >> 8);
+  writeb(ata0, _DEVREG_ATA_CYHIGH, offset >> 16);
+  writeb(ata0, _DEVREG_ATA_DRIVE,  (offset >> 24) | 0xe0);
+  writeb(ata0, _DEVREG_ATA_STATUS, 0x20);
+  while ((readb(ata0, _DEVREG_ATA_STATUS) & 0xc0) != 0x40);
+  for (int i = 0; i < SECTSZ / sizeof(uint32_t); i++) 
+    ((uint32_t *)dst)[i] = readl(ata0, _DEVREG_ATA_DATA);
+  printf("read completed!\n");
+}
