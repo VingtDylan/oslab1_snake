6c3d14ae7c91c0b869f662f9e55d9ca6a152b13b
tracer <tracer@tracer.org>
2018-04-02 21:07:46 +0800

    > run cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game/game.c
+++ src/game/game.c
@@ -221,7 +221,7 @@ void snake_move(){
 
 void get_food(){
   for(int i=0;i<foo;i++){
-     if(snake.x[0]>=food[i].fx-4&&snake.x[0]<=food[i].fx+4&&snake.y[0]>=food[i].fy-4&&snake.y[0]<=food[i].fy+4){
+     if(snake.x[0]>=food[i].fx&&snake.x[0]<=food[i].fx+4&&snake.y[0]>=food[i].fy&&snake.y[0]<=food[i].fy+4){
           food[i].flag=false;
           snake.foods++;
           if(snake.foods>=20)
