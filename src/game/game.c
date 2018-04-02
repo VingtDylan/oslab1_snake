0522827d330d08bdb70884ef52d5a6a336addba8
tracer <tracer@tracer.org>
2018-04-02 21:48:10 +0800

    > run cyh Linux 161240005 4.13.0-37-generic #42~16.04.1-Ubuntu SMP Wed Mar 7 16:02:25 UTC 2018 i686 i686 i686 GNU/Linux

--- src/game/game.c
+++ src/game/game.c
@@ -149,7 +149,7 @@ void draw_screen(){
      uint32_t pixel=purple;
      for(int x=0;x<screen.width;x++){
         for(int y=0;y<screen.height;y++){
-           draw_rect(&pixel,x,y,1,1);
+           draw_rect(&background,x,y,1,1);
            if(x>270&&x<345&&y>0&&y<340)
               if((y-190)>=2*(x-270)-10&&(y-190)<=2*(x-270)+10)
                    draw_rect(&pixel,x,y,1,1);
